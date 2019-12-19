0c330a699bad5b7ee944e403727ae07392d6df41
amrebrahem22 <amrebrahem226@gmail.com>
2019-12-19 06:50:10 +0200

    added load more button

--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -1,11 +1,13 @@
 from django.db.models import Q
 from rest_framework import permissions
 from rest_framework import generics
-from .serializers import TweetSerializer
 from tweets.models import Tweet
+from .serializers import TweetSerializer
+from .pagination import TweetsPagination
 
 class TweetList(generics.ListAPIView):
 	serializer_class = TweetSerializer
+	pagination_class = TweetsPagination
 
 	def get_queryset(self):
 		queryset = Tweet.objects.all().order_by('-timestamp')
